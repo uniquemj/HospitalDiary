a2a3c99e501ff69eb8b0d16fcba622a98fed7c31
Unique Maharjan <maharjanyunik77@gmail.com>
2023-11-25 18:08:45 +0545

    DB Configuration

--- hospitalsystem/settings.py
+++ hospitalsystem/settings.py
@@ -80,9 +80,14 @@ WSGI_APPLICATION = 'hospitalsystem.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default="sqlite:///"+ os.path.join(BASE_DIR/"db.sqlite3")
-    )
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'NAME':os.environ.get('DB_NAME'),
+        'USER':os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASS'),
+        'HOST':os.environ.get('DB_HOST'),
+        'PORT':'3306'
+    }
 }
 
 
